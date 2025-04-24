666e636294f907346f0d7bb1e04718403741ac5e
AdamFromdust <adam.fromdust.todust@gmail.com>
2025-04-24 16:20:35 +0200

    Ch 15 Authentication

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,8 +4,30 @@ import {z} from 'zod';
 import { revalidatePath } from 'next/cache';
 import postgres from 'postgres';
 import { redirect } from 'next/navigation';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.message) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          console.log(error.message)
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
+}
 
 const FormSchema = z.object({
   id: z.string(),
